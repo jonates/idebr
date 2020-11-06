0c217c31e9abc75ce734ccd289547a11ca797375
Jonatas Silva do Espirito Santo <jonates@gmail.com>
2020-11-06 01:44:13 -0300

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -18,3 +18,17 @@
 #'
 #' @source ideb_brasil_fundamental_iniciais
 'ideb_brasil_fundamental_iniciais'
+#'
+#' ideb_escolas_ensino_medio
+#'
+#' @format ideb_escolas_ensino_medio
+#'
+#' @source ideb_escolas_ensino_medio
+'ideb_escolas_ensino_medio'
+#'
+#' ideb_escolas_fundamental_finais
+#'
+#' @format ideb_escolas_fundamental_finais
+#'
+#' @source ideb_escolas_fundamental_finais
+'ideb_escolas_fundamental_finais'
